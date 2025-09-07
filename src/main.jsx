99f7e5289bacaf0eb61709a3233dca35775b3ff5
psychomagnet <inspiration.provider@gmail.com>
2025-09-07 15:25:54 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './ui/App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./ui/App.jsx";
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById("root")).render(<App />);
