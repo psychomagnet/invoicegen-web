5d0503085fe85600741bcab2b709639da041f442
psychomagnet <inspiration.provider@gmail.com>
2025-09-07 15:56:48 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./ui/App.jsx";
+import ErrorBoundary from "./ui/ErrorBoundary.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
